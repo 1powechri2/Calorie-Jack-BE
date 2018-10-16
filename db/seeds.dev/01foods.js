ef9a042358398a9871797a5a9189fde6c158726b
Chris Powell <chris.ian.powell@gmail.com>
2018-10-16 12:12:19 -0600

    Adds meals query to server

--- db/seeds.dev/01foods.js
+++ db/seeds.dev/01foods.js
@@ -1,18 +1,18 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('mealFoods').del()
+  return knex('meal_foods').del()
     .then(() => knex('meals').del())
     .then(() => knex('foods').del())
 
     .then(() => {
       return Promise.all([
         knex('foods').insert({
-          name: 'Banana', calories: 150 }, 'id'),
+          id: 1, name: 'Banana', calories: 150 }, 'id'),
         knex('foods').insert({
-          name: 'apple', calories: 220 }, 'id'),
+          id: 2, name: 'apple', calories: 220 }, 'id'),
         knex('foods').insert({
-          name: 'Granola Bar', calories: 300 }, 'id')
+          id: 3, name: 'Granola Bar', calories: 300 }, 'id')
         .then(() => console.log('Seeding Foods complete'))
         .catch(error => console.log(`Error seeding foods data: ${error}`))
     ])
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
+const pry = require('pryjs');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.set('port', process.env.PORT || 8080);
@@ -14,7 +16,29 @@ app.locals.title = 'calorie_jack';
 app.get('/', (request, response) => {
   response.send('Hello, Dolly');
 });
+//
+// app.get('/api/v1/meals', (request, response) => {
+//   database.raw(`
+//       SELECT meals.id, meals.name, array_to_json
+//       (array_agg(json_build_object('id', foods.id, 'name', foods.name, 'calories', foods.calories)))
+//       AS foods
+//       FROM meals
+//       JOIN meal_foods ON meals.id = meal_foods.meal_id
+//       JOIN foods ON meal_foods.food_id = foods.id
+//       GROUP BY meals.id`)
+//   .then(meals => {
+//     response.status(200).json(meals)
+//   })
+// })
+
+app.get('/api/v1/meals', (request, response) => {
+  database.select('meals.id', 'meals.name', 'foods.id', 'foods.name', 'foods.calories')
+  .from('meals')
+  .join('meal_foods', ())
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
+
+module.exports = app;
