a5e08f979d8602d9f562870f4f1e0e17a94e1c04
Chris Powell <chris.ian.powell@gmail.com>
2018-10-20 14:52:17 -0600

    Adds more CORS stuff

--- server.js
+++ server.js
@@ -10,12 +10,19 @@ const database = require('knex')(configuration);
 let mealRow;
 let foodRow;
 
-app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      res.header('Access-Control-Allow-Methods', 'PATCH, POST, GET, DELETE, OPTIONS');
-         next();
-});
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
+};
+app.use(allowCrossDomain);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
