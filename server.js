1b7f0fe134ef53a2ff5ac14e3e734d68dbc794d9
Chris Powell <chris.ian.powell@gmail.com>
2018-10-20 14:22:39 -0600

    Adds change to CORS problem

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const cors = require('cors');
+require('dotenv').config();
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
@@ -10,7 +11,12 @@ const database = require('knex')(configuration);
 let mealRow;
 let foodRow;
 
-app.use(cors());
+app.use(function(req, res, next) {
+      res.header(“Access-Control-Allow-Origin”, “*”);
+      res.header(“Access-Control-Allow-Headers”, “Origin, X-Requested-With, Content-Type, Accept”);
+      res.header(‘Access-Control-Allow-Methods’, ‘PATCH, POST, GET, DELETE, OPTIONS’);
+         next();
+   });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.set('port', process.env.PORT || 8080);
