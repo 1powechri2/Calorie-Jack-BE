951d291440c08f2cc6dfc00ffbc6bec471a70960
Chris Powell <chris.ian.powell@gmail.com>
2018-10-20 14:29:06 -0600

    Adds another change to CORS stuff

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ let mealRow;
 let foodRow;
 
 app.use(function(req, res, next) {
-      res.header(“Access-Control-Allow-Origin”, “*”);
-      res.header(“Access-Control-Allow-Headers”, “Origin, X-Requested-With, Content-Type, Accept”);
-      res.header(‘Access-Control-Allow-Methods’, ‘PATCH, POST, GET, DELETE, OPTIONS’);
+      req.header(“Access-Control-Allow-Origin”, “*”);
+      req.header(“Access-Control-Allow-Headers”, “Origin, X-Requested-With, Content-Type, Accept”);
+      req.header(‘Access-Control-Allow-Methods’, ‘PATCH, POST, GET, DELETE, OPTIONS’);
          next();
    });
 app.use(bodyParser.json());
