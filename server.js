2529db5d6c49010bdb0038e57d2d5c00a07fb6a4
Chris Powell <chris.ian.powell@gmail.com>
2018-10-20 21:51:16 -0600

    Adds more minor adjustments to the git status
    f

--- server.js
+++ server.js
@@ -10,17 +10,17 @@ const database = require('knex')(configuration);
 let mealRow;
 let foodRow;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.set('port', process.env.PORT || 8080);
+app.locals.title = 'calorie_jack';
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "DELETE, PATCH, PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
-app.set('port', process.env.PORT || 8080);
-app.locals.title = 'calorie_jack';
 
 app.get('/', (request, response) => {
   response.send('Hello, Dolly');
